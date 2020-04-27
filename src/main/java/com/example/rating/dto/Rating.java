2f28829c4cb0c1cf309e54dc52a081380536940d
salman.khandu <salman.khandu@peaas.co>
2020-04-27 22:47:32 +0530

    rating service

---
+++ src/main/java/com/example/rating/dto/Rating.java
@@ -0,0 +1,33 @@
+package com.example.rating.dto;
+public class Rating {
+    private Long id;
+    private Long bookId;
+    private int stars;
+	public Rating(Long id, Long bookId, int stars) {
+		super();
+		this.id = id;
+		this.bookId = bookId;
+		this.stars = stars;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getBookId() {
+		return bookId;
+	}
+	public void setBookId(Long bookId) {
+		this.bookId = bookId;
+	}
+	public int getStars() {
+		return stars;
+	}
+	public void setStars(int stars) {
+		this.stars = stars;
+	}
+    
+    
+	
+}
