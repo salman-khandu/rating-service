23ad6ff5c80ce09f62d7047e65de518e8bee7065
salman.khandu <salman.khandu@peaas.co>
2020-04-29 00:09:49 +0530

    rating service changes

--- src/main/java/com/example/rating/RatingApplication.java
+++ src/main/java/com/example/rating/RatingApplication.java
@@ -3,11 +3,9 @@ package com.example.rating;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrix
 public class RatingApplication {
 
 	public static void main(String[] args) {
